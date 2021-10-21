e07117da5700046c0aa463d5213eb6cb3de88a74
Jorgevsi99 <68628181+Jorgevsi99@users.noreply.github.com>
2021-10-21 16:01:04 -0500

    Update regfal.m

--- octave/regfal.m
+++ octave/regfal.m
@@ -1,51 +1,60 @@
 %REGULA FALSI
 %   REGFAL(FUN,A,D,T,ITER) llenar
 %       [ra,datos] = regfal(fun, a, b, tol, iter)
-%       ra = raiz de la función
-%       datos = todas las variables de la ejecución
-%       fun = la función a tratar
-%       [a,b] intervalo
+%       ra = raiz de la funciÃ³n
+%       datos = todas las variables de la ejecuciÃ³n
+%       fun = la funciÃ³n a tratar
+%       [an,bn] intervalo
 %       tol = tolerancia
-%       iter = número máximo de iteraciones
+%       iter = nÃºmero mÃ¡ximo de iteraciones
 
-function [ra,datos,j] = newton(fun,a,b,tol=0.01,iter=1000)
+function [ra,datos,j] = regfal(fun,an,bn,tol=0.01,iter=1000)
   j=2;
   ea=100;
   f=fun;
-  c=a;
-  d=b;
+  u=f(an);
+  v=f(bn);
+  a=an;
+  b=bn;
   p=a;
-  x(1)=p; %Toma como primer valor el extremo izquierdo del intervalo
+##  x(1)=p; %Toma como primer valor el extremo izquierdo del intervalo
   
   while ea>tol
     if j>1
       %almacenamos los primeros datos
-      datos(j-1,1)=c;
-      datos(j-1,2)=d;
+      datos(j-1,1)=a;
+      datos(j-1,2)=b;
+            
+      %Formula para el punto en regla falsa
+      p=a-(u*(b-a)/(v-u));
       
-      %Calculamos el porcentaje de error
-      ea=abs((c-d)/c)*100;
+      %Calculo del error
+      ea=f(p);
+      w=ea;
       datos(j-1,3)=ea;
       
-      %Formula para el punto en regla falsa
-      p=c-f(c)*(d-c)/(f(d)-f(c));
-      
-      if f(c)*f(p)<0 %Analiza si hay cambio de signo para acortar el intervalo
+      if u*w<0 %Analiza si hay cambio de signo para acortar el intervalo
         %Reemplaza el extremo b del intervalo por el punto p
-        d=p;
-        ra=d;
+        a=an;
+        b=p;
+        %ra=b;
       else
         %Reemplaza el otro extremo del intervalo
-        c=p;
-        ra=c;
+        a=p;
+        b=bn;
+        %ra=a;
       end
+      u=f(a);
+      v=f(b);
+      j=j+1;
+      ra=p;
     elseif j>iter
-      msgID = 'NR:iteraciones';
-      msg = 'Gran número de iteraciones, posiblemente el método divergió';
+      msgID = 'REGFAL:iteraciones';
+      msg = 'Gran nÃºmero de iteraciones, posiblemente el mÃ©todo divergiÃ³';
       error(msgID,msg);
     endif
     if ea<tol
       break;
     endif
-    j=j+1;
 end
+%ra=p;
